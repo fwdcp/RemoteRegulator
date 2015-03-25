8b97068cf72e5a00d5aaabf677f75bbfcc373f42
thesupremecommander <thesupremecmdr@gmail.com>
2015-03-25 19:55:53 -0400

    Flesh out the extension a bit more for views.

--- extension.js
+++ extension.js
@@ -1,6 +1,45 @@
 module.exports = function(nodecg) {
+    nodecg.declareSyncedVar({
+        name: 'clients',
+        initialValue: []
+    });
+
+    var autocompleteRequests = [];
+
+    var clients = {};
+    var dashboards = {};
+
+    function getClientSocketIDFromSteam(steam) {
+        return underscore.findKey(clients, function(client) {
+            return client.steam == steam;
+        })
+    }
+
+    function updateClients() {
+        underscore.each(clients, function(client, socketID) {
+            if (client) {
+                if (!client.following) {
+                    clients[socketID].following = "0";
+                }
+
+                clients[socketID].authorized = (!nodecg.bundleConfig.authorizedClients && client.steam) || underscore.contains(nodecg.bundleConfig.authorizedClients, client.steam);
+            }
+        });
+
+        nodecg.variables.clients = underscore.where(clients, {authorized: true});
+    }
+
     var io = nodecg.getSocketIOServer().of('/RemoteRegulator');
 
     io.on('connection', function(socket) {
+        socket.on('clientUpdate', function(data) {
+            if (!clients[socket.id]) {
+                clients[socket.id] = {};
+            }
+
+            underscore.extend(clients[socket.id], data);
+
+            updateClients();
+        });
     });
 }
