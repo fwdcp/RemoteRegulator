c66de8520db8e6da96bb2121c9e52b87a7e298fa
thesupremecommander <thesupremecmdr@gmail.com>
2015-03-25 19:56:10 -0400

    Start working on the panel a bit more.

--- dashboard/panel.js
+++ dashboard/panel.js
@@ -1,3 +1,17 @@
-$(document).ready(function() {
+var expose = {
+    consoles: []
+};
 
+nodecg.declareSyncedVar({
+    name: 'clients',
+    initialValue: [],
+    setter: function(data) {
+
+    }
 });
+
+rivets.formatters.prepend = function(value, prepend) {
+    return prepend + value;
+};
+
+rivets.bind($('.RemoteRegulator'), {data: expose});
